b9c53c1e5d6e4e62893b79c3ab4f03ef9d3a352e
sefp-test <mail-for-rails-tutorial-sefp@mail.ru>
2016-08-02 14:28:09 +0300

    Inititalize repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'f0e171aef03269f70bcdece19b7f0446490854993ce89bb612b490f11979b40df4f2351d5c749d1842a479105a8cbc36b2f362d2e2440d70b0add783447bc4e9'
